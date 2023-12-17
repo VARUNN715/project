3d42a25c579fbaa35c246d810befd5ca5370500f
VARUNN715 <118555997+VARUNN715@users.noreply.github.com>
2023-12-17 18:09:39 +0530

    Add files via upload

---
+++ Project.java
@@ -0,0 +1,41 @@
+
+import java.util.Scanner;
+
+public class Project {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int myNum = (int)(Math.random()*100);
+        int num = 0;
+       try{ do{
+            System.out.println("guess your number : ");
+            num = sc.nextInt();
+
+            if(myNum == num){
+                System.out.println("you guessed it right ");
+                exit();
+                break;
+            }
+            else if(myNum > num && num>=0){
+                System.out.println("tru again the number is higher");
+            }
+            else if(myNum < num && num>=0){
+                System.out.println("try again the number is lesser");
+            }
+            else{
+                quit();
+            }
+        } while(num >= 0);
+    } catch(Exception e){
+       // System.err.println(e);
+    }
+    sc.close();
+        
+    }
+
+    private static void exit() {
+        System.out.println("game over you won");
+    }
+    private static void quit() {
+        System.out.println("you quit!!");
+    }
+}
